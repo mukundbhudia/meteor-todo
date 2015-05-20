323097d66ac59de5e3e3f327f5991770c4808f01
Mukund Bhudia <mukund_bhudia@hotmail.com>
2015-05-20 19:35:05 +0100

    Added ability to save a new task

--- simple-todos.js
+++ simple-todos.js
@@ -8,4 +8,23 @@ if (Meteor.isClient) {
             return Tasks.find({});
         }
     });
+
+    Template.body.events({
+        "submit .new-task": function (event) {
+            //Function called on submission of new task form
+            console.debug(event);
+            var text = event.target.text.value;
+
+            Tasks.insert({
+                text: text,
+                createdAt: new Date() //now
+            });
+
+            //Clear the form
+            event.target.text.value = "";
+
+            //Prevent default form submit
+            return false;
+        }
+    });
 }

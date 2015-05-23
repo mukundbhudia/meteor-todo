12f87912ca132860d28e72fce1043335024fc527
Mukund Bhudia <mukund_bhudia@hotmail.com>
2015-05-23 19:31:39 +0100

    Implemented login to the app

--- simple-todos.js
+++ simple-todos.js
@@ -4,51 +4,56 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
-        tasks: function () {
+        tasks: function() {
             if (Session.get("hideCompleted")) {
                 //If the user has checked hide completed then we filter tasks
-                return Tasks.find({checked: {$ne: true}},
-                    {sort: {createdAt: -1}});
-                } else {
-                    return Tasks.find({}, {sort: {createdAt: -1}});
-                }
-            },
-            hideCompleted: function() {
-                return Session.get("hideCompleted");
-            },
-            incompleteCount: function() {
-                return Tasks.find({checked: {$ne: true}}).count();
+                return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+            } else {
+                return Tasks.find({}, {sort: {createdAt: -1}});
             }
-        });
+        },
+        hideCompleted: function() {
+            return Session.get("hideCompleted");
+        },
+        incompleteCount: function() {
+            return Tasks.find({checked: {$ne: true}}).count();
+        }
+    });
 
-        Template.body.events({
-            "submit .new-task": function(event) {
-                //Function called on submission of new task form
-                console.debug(event);
-                var text = event.target.text.value;
+    Template.body.events({
+        "submit .new-task": function(event) {
+            //Function called on submission of new task form
+            console.debug(event);
+            var text = event.target.text.value;
 
-                Tasks.insert({
-                    text: text,
-                    createdAt: new Date() //now
-                });
+            Tasks.insert({
+                text: text,
+                createdAt: new Date(),              //now
+                owner: Meteor.userId(),             //_id of user logged in
+                username: Meteor.user().username    //username of logged in user
+            });
 
-                //Clear the form
-                event.target.text.value = "";
+            //Clear the form
+            event.target.text.value = "";
 
-                //Prevent default form submit
-                return false;
-            },
-            "change .hide-completed input": function (event) {
-                Session.set("hideCompleted", event.target.checked);
-            }
-        });
+            //Prevent default form submit
+            return false;
+        },
+        "change .hide-completed input": function(event) {
+            Session.set("hideCompleted", event.target.checked);
+        }
+    });
 
-        Template.task.events({
-            "click .toggle-checked": function() {
-                Tasks.update(this._id, {$set: {checked: ! this.checked}});
-            },
-            "click .delete": function() {
-                Tasks.remove(this._id);
-            }
-        });
-    }
+    Template.task.events({
+        "click .toggle-checked": function() {
+            Tasks.update(this._id, {$set: {checked: ! this.checked}});
+        },
+        "click .delete": function() {
+            Tasks.remove(this._id);
+        }
+    });
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
+}

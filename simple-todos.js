bcd280123a4aa51dc2944a2c8fb9161bd0390271
Mukund Bhudia <mukund_bhudia@hotmail.com>
2015-05-23 19:14:33 +0100

    Hide complete tasks, incomplete task badge
    Use of session for temporary reactive state storage

--- simple-todos.js
+++ simple-todos.js
@@ -5,35 +5,50 @@ if (Meteor.isClient) {
     // This code only runs on the client
     Template.body.helpers({
         tasks: function () {
-            return Tasks.find({}, {sort: {createdAt: -1}});
-        }
-    });
+            if (Session.get("hideCompleted")) {
+                //If the user has checked hide completed then we filter tasks
+                return Tasks.find({checked: {$ne: true}},
+                    {sort: {createdAt: -1}});
+                } else {
+                    return Tasks.find({}, {sort: {createdAt: -1}});
+                }
+            },
+            hideCompleted: function() {
+                return Session.get("hideCompleted");
+            },
+            incompleteCount: function() {
+                return Tasks.find({checked: {$ne: true}}).count();
+            }
+        });
 
-    Template.body.events({
-        "submit .new-task": function (event) {
-            //Function called on submission of new task form
-            console.debug(event);
-            var text = event.target.text.value;
+        Template.body.events({
+            "submit .new-task": function(event) {
+                //Function called on submission of new task form
+                console.debug(event);
+                var text = event.target.text.value;
 
-            Tasks.insert({
-                text: text,
-                createdAt: new Date() //now
-            });
+                Tasks.insert({
+                    text: text,
+                    createdAt: new Date() //now
+                });
 
-            //Clear the form
-            event.target.text.value = "";
+                //Clear the form
+                event.target.text.value = "";
 
-            //Prevent default form submit
-            return false;
-        }
-    });
+                //Prevent default form submit
+                return false;
+            },
+            "change .hide-completed input": function (event) {
+                Session.set("hideCompleted", event.target.checked);
+            }
+        });
 
-    Template.task.events({
-        "click .toggle-checked": function() {
-            Tasks.update(this._id, {$set: {checked: ! this.checked}});
-        },
-        "click .delete": function() {
-            Tasks.remove(this._id);
-        }
-    });
-}
+        Template.task.events({
+            "click .toggle-checked": function() {
+                Tasks.update(this._id, {$set: {checked: ! this.checked}});
+            },
+            "click .delete": function() {
+                Tasks.remove(this._id);
+            }
+        });
+    }
